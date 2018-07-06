9856412838c57785b93414e8a40f7493760bb0aa
Fisnik Canaj <fisnikcanaj1@github.com>
2018-07-06 10:19:43 +0200

    Header done

--- gulp/task/tools.js
+++ gulp/task/tools.js
@@ -6,17 +6,19 @@ const gulp = require('gulp'),
     image = require('gulp-image'),
     minify = require('gulp-minifier');
 
-gulp.task('styles', function () {
-    gulp.src('./app/assets/css/**/*.css')
-        .pipe(postcss([autoPrefixer]))
-        .pipe(gulp.dest('./app/temp/css/'));
-});
+
 
 gulp.task('sass', function () {
     gulp.src('./app/assets/scss/**/*.scss')
         .pipe(errorHendler())
         .pipe(sass())
         .pipe(gulp.dest("./app/assets/css"));
+});
+
+gulp.task('styles', function () {
+    gulp.src('./app/assets/css/**/*.css')
+        .pipe(postcss([autoPrefixer]))
+        .pipe(gulp.dest('./app/temp/css/'));
 });
 
 gulp.task('html', function () {
